de2135469221a2a2d275f90205f7affe23855d91
betazetadev <hi@betazetadev.com>
2023-03-01 23:03:55 +0100

    Lista completa de películas

---
+++ lib/main.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+
+String fetchAllFilms = r"""
+  query FetchAllFilms {
+    film {
+      title
+      fulltext
+      description
+    }
+  }
+""";
+
+void main() async {
+  await initHiveForFlutter();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final HttpLink httpLink = HttpLink(
+      'http://10.0.2.2:8080/v1/graphql',
+    );
+    final AuthLink authLink = AuthLink(
+      getToken: () async => 'Bearer ',
+      // OR
+      // getToken: () => 'Bearer <YOUR_PERSONAL_ACCESS_TOKEN>',
+    );
+    final Link link = authLink.concat(httpLink);
+    ValueNotifier<GraphQLClient> client = ValueNotifier(
+      GraphQLClient(
+        link: link,
+        // The default store is the InMemoryStore, which does NOT persist to disk
+        cache: GraphQLCache(store: HiveStore()),
+      ),
+    );
+    return GraphQLProvider(
+        client: client,
+        child: MaterialApp(
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: const MyHomePage(title: 'Flutter Demo Home Page'),
+        ));
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+          child: Query(
+        options: QueryOptions(
+          document: gql(fetchAllFilms),
+        ),
+        builder: (QueryResult result,
+            {VoidCallback? refetch, FetchMore? fetchMore}) {
+          if (result.hasException) {
+            return Text(result.exception.toString());
+          }
+
+          if (result.isLoading) {
+            return const Text('Loading');
+          }
+
+          // it can be either Map or List
+          List films = result.data!['film'];
+
+          return ListView.builder(
+            itemCount: films.length,
+            itemBuilder: (context, index) {
+              final film = films[index];
+              return ListTile(
+                title: Text(film['title']),
+                subtitle: Text(film['description']),
+              );
+            },
+          );
+        },
+      )), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
